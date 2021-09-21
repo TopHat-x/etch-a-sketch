c2239789c6728f25aa54487bb963e4e9f4010253
TopHat-x <Brennan.Berryman@outlook.com>
2021-09-21 15:13:04 -0600

    Fully functional!

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ let gridColumn;
 let gridSquare;
 
 function createGrid(gridSize){
+    while (gridContainer.firstChild){
+        gridContainer.removeChild(gridContainer.firstChild);
+    }    
+
     for (column = 1; column <= gridSize; column++){
         gridColumn = document.createElement('div');
         gridColumn.className = 'grid-column';
@@ -10,22 +14,51 @@ function createGrid(gridSize){
         for (row = 1; row <= gridSize; row++){
             gridSquare = document.createElement('div');
             gridSquare.className = 'grid-square';
+            gridSquare.style.backgroundColor = "white";
             gridColumn.appendChild(gridSquare);
         }
     }
+    const gridSquares = document.querySelectorAll('.grid-square');
+    gridSquares.forEach(gridSquare => gridSquare.addEventListener("mouseover", changeBgColor));
 }
 
 createGrid(16);
 
-const gridSquares = document.querySelectorAll('.grid-square');
-gridSquares.forEach(gridSquare => gridSquare.addEventListener("mouseover", changeBgColor));
 
 function changeBgColor(){
-    this.style.backgroundColor = "black";
+    if (this.style.backgroundColor === "white"){
+        let r = randomInt(256);
+        let g = randomInt(256);
+        let b = randomInt(256);
+    
+        this.style.backgroundColor = "rgb(" + r + ", " + g + ", " + b + ")";
+    } else if (this.style.backgroundColor !== "rgb(0, 0, 0)"){
+        let rgbValues = getRGBValues(this.style.backgroundColor);
+        let r = parseInt(rgbValues[0]);
+        let g = parseInt(rgbValues[1]);
+        let b = parseInt(rgbValues[2]);
+
+        r = Math.floor(r - 25.5);
+        g = Math.floor(g - 25.5);
+        b = Math.floor(b - 25.5);
+
+        if (r < 0){
+            r = 0;
+        }
+
+        if (g < 0){
+            g = 0;
+        }
+
+        if (b < 0){
+            b = 0;
+        }
+
+        this.style.backgroundColor = "rgb(" + r + ", " + g + ", " + b + ")";
+    }
 }
 
 function promptGridSize(){
-    console.log("promptGridSize");
     let gridSize = undefined;
 
     while (typeof gridSize !== "number"){
@@ -42,6 +75,15 @@ function promptGridSize(){
         createGrid(gridSize);
     }
 }
+
+function randomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+function getRGBValues(str) {
+    var vals = str.substring(str.indexOf('(') +1, str.length -1).split(', ');
+    return vals;
+  }
 
 const resetButton = document.querySelector("#reset-button");
 resetButton.addEventListener('click', () => {promptGridSize()});
